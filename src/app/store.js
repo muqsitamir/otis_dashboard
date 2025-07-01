de07ecff39670829b4cea2b473085549699edcc8
muqsitamir <islammuqsit7@gmail.com>
2025-07-02 01:06:56 +0500

    Git init

---
+++ src/app/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import filterReducer from '../features/filters/filterSlice';
+// import organizationReducer from '../features/organization/organizationSlice';
+// import eventsReducer from '../features/events/eventsSlice'
+// import pieChartReducer from '../features/piechart/pieChartSlice'
+// import lineChartReducer from '../features/linechart/lineChartSlice'
+// import mapsReducer from '../features/maps/mapsSlice'
+// import userReducer from '../features/userinfo/userSlice'
+// import cameraReducer from "../features/cameras/cameraSlice";
+// import siteDataReducer from "../reusable_components/site_data/siteDataSlice";
+//
+// export const store = configureStore({
+//   reducer: {
+//     filters: filterReducer
+//
+//   },
+// });
